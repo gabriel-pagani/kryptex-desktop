ea6ef5c6c622975dc29d6277f5d03c19e34c523e
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-07 15:04:40 -0300

    Test controllers

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
-import flet as ft
+# import flet as ft
 
 
-def main(page: ft.Page):
-    None
+# def main(page: ft.Page):
+#     None
 
 
-ft.run(main, assets_dir="assets")
+# ft.run(main, assets_dir="assets")
+
+from controllers.user import User
+
+obj = User(username='admin', master_password='1234')
+print(obj.create_user())
+print(obj.delete_user())
