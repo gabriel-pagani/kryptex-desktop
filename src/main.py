d10827382e005c72a61e278f09593f1ec4cf31db
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-07 22:44:45 -0300

    Refactor main function in main.py to enable application execution

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
-# import flet as ft
+import flet as ft
 
 
-# def main(page: ft.Page):
-#     None
+def main(page: ft.Page):
+    None
 
 
-# ft.run(main, assets_dir="assets")
-
-from controllers.user import User
-
-new_user = User.create(username='admin', master_password='1234') 
+ft.run(main, assets_dir="assets")
