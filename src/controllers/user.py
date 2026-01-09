61e70c9eabf304ae9e0761339b1f02adffb548e7
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-08 23:48:08 -0300

    Add create method to User class for user registration

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,5 +1,7 @@
+from os import urandom
 from typing import Optional
 from database.connection import execute_query
+from utils.cryptor import generate_hash
 
 
 class User:
@@ -14,6 +16,30 @@ class User:
         self.salt = salt
         self.username = username
         self.master_password_hash = master_password_hash
+
+    @classmethod
+    def create(cls, username: str, master_password: str) -> Optional['User']:
+        try:
+            salt = urandom(32)
+            master_password_hash = generate_hash(master_password)
+
+            response = execute_query(
+                "INSERT INTO users (salt, username, master_password_hash) VALUES (?, ?, ?) RETURNING *",
+                (salt, username, master_password_hash)
+            )
+
+            if response != []:
+                return cls(
+                    id=response[0][0],
+                    salt=response[0][1],
+                    username=response[0][2],
+                    master_password_hash=response[0][3]
+                )
+            return None
+            
+        except Exception as e:
+            print(f"exception-on-create: {e}")
+            return None
 
     @classmethod
     def get(cls, id: int) -> Optional['User']:
